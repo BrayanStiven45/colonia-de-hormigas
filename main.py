4015f946319e675712a27f3ba5621b545dc3d2be
BrayanStiven45 <bravalen45@gmail.com>
2025-04-10 00:23:22 -0500

    Se actualizo el informe, tambien se cambiaron las esplicaciones a cada una de las instancias en el codigo para que se mejor legible y en lenguaje español

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ tsp = TSP()
 # Load the TSP instance from a file using tsplib95 
 # and set the parameters for the algorithm path_file, n_ants, n_iters, alpha, beta, ro
 # tsp.load_initial_variables("initializer_files/kroA100.tsp", 50, 1000, 1, 1, 0.5)
-tsp.load_initial_variables("initializer_files/berlin52.tsp", 80, 2000, 1, 1.5, 0.2)
+tsp.load_initial_variables("initializer_files/berlin52.tsp", 90, 3000, 1, 1.5, 0.2)
 
 best_path, best_lenght, total_time = tsp.solve()
 # distances = tsp.get_distances()
